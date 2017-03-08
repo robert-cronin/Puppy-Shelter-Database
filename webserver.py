b4efe7376d698d49dee60264e07b1de1d99fa5f7
Robbie Cronin <robert.cronin@uqconnect.edu.au>
2017-03-08 15:42:29 +1000

    Add Hola functionality

--- webserver.py
+++ webserver.py
@@ -14,6 +14,17 @@ class webserverHandler(BaseHTTPRequestHandler):
                 print output
                 return
 
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>&#161 Hola ! <a href = '/hello' > Back to Hello</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
         except IOError:
             self.send_error(404, "File Not Fount %s" % self.path)
 
